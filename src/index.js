c57bba4b05d8fea85ed2ae4cd83f2bbf616cc912
Alexey Zyukov <aalex64@ukr.net>
2021-08-25 08:09:34 +0300

    замена версии

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './sass/main.scss';
+import '@pnotify/core/dist/BrightTheme.css';
+import { alert, info, success, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
+
+alert({
+    text: 'It is me'
+});
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,28 +1,61 @@
+'use strict'
 
+const input = document.querySelector('#text-input');
+const result = document.querySelector('.input-data');
+const countryList = document.querySelector('.container');
+// let countryName = "";
+let countryArrayLength = null;
 
-const input = document.querySelector('#text-input')
-const result = document.querySelector('.input-data')
-let countryName = ""
-console.log(countryName);
-
-input.value = 'test'
-
-console.log(result);
-function onInputSearch(event) {
-     countryName = input.value;
-     console.log(countryName);
-}
-fetch(`https://restcountries.eu/rest/name/${countryName}`)
-     .then(console.log(countryName))
-     .then(response => response.json())
-    .then(response => console.log(response))
-    .then(data => console.log(data))
-     .catch(err => console.log(err));
-    
 input.addEventListener('input', onInputSearch);
+//console.log(countryArrayLength);
 
 
-
-// function createCountryCard() {
-     
+// function onInputSearch() {
+//      countryName = input.value;
+//      const countryList = fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
+//           .then((response) => response.json())
+//           .then(json => {
+//                console.log(json);
+//                countryArrayLength = json.length;
+//                callbackFn(countryArrayLength);
+//                console.log(countryArrayLength);
+               
+//         //Once succcessful callback return you can find length of number of item
+//                return countryArrayLength;
+//           })
+//           .catch(err => console.log(err));
+//      return countryList;
 // }
+// // const resultOfSearch = onInputSearch();
+// // console.log(resultOfSearch);
+
+// function callbackFn(data) {
+//      console.log(data);
+// }
+// callbackFn();
+
+function onInputSearch() {
+     const countryName = input.value;
+     fetchCountriesList(countryName);
+}
+
+function fetchCountriesList(name) {
+    return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+          .then(response => response.json())
+          .then(json => {
+               console.log(json);
+               countryArrayLength = json.length;
+               console.log(countryArrayLength);
+        //Once succcessful callback return you can find length of number of item
+               return countryArrayLength;
+          })
+          .then(result => createSingleCountryCard(result))
+          //.then(result => console.log(result))
+          .catch(err => console.log(err));
+}
+
+function createSingleCountryCard(obj) {
+     const countryCard = `<li>${obj.name}</li>`;
+     countryList.insertAdjacentHTML('beforeend', countryCard);
+};
+
