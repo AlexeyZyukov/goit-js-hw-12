91ed248fd10342de83dd0d238e4724e7a1387374
Alexey Zyukov <aalex64@ukr.net>
2021-08-31 21:10:57 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -6,33 +6,52 @@ const countryList = document.querySelector('.container');
 // let countryName = "";
 let countryArrayLength = null;
 
+console.log(input.value);
+
 input.addEventListener('input', onInputSearch);
 //console.log(countryArrayLength);
-
-
+// countryList.insertAdjacentElement('beforeend',)
 
 function onInputSearch() {
      const countryName = input.value;
-     fetchCountriesList(countryName);
-}
+     console.log(countryName);
+     //fetchCountryByName(countryName).then(createSingleCountryCard);
+     fetchCountryByName(countryName).then(onSuccessFetch);
+     console.log(fetchCountryByName(countryName))
+};
+// // fetchCountryByName('Rus').then(onSuccessFetch);
 
-function fetchCountriesList(name) {
-    return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+function fetchCountryByName(name) {
+     return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
           .then(response => response.json())
-          .then(json => {
-               console.log(json);
-               countryArrayLength = json.length;
-               console.log(countryArrayLength);
-        //Once succcessful callback return you can find length of number of item
-               return countryArrayLength;
-          })
-          .then(result => createSingleCountryCard(result))
-          //.then(result => console.log(result))
-          .catch(err => console.log(err));
-}
+          // .catch(err => console.log(err));
+};
+// console.log(fetchCountryByName());
 
 function createSingleCountryCard(obj) {
-     const countryCard = `<li>${obj.name}</li>`;
-     countryList.insertAdjacentHTML('beforeend', countryCard);
+     return obj.map(countryName => {
+         return `<li>${countryName.name}</li>`
+     }).join('');     
 };
+
+function onSuccessFetch(country) {
+     createSingleCountryCard(country);
+     //console.log(country);
+}
+
+
+
+
+// function createSingleCountryCard(countryName) {
+//      const singleCountryCard = `<li>${countryName.name}</li>`;
+//      countryList.innerHTML = singleCountryCard;
+// };
+// function error(error) {
+//   alert('Упс, что-то пошло не так и мы не нашли вашего покемона!');
+// }
+
+//countryList.insertAdjacentHTML('beforeend', createSingleCountryCard);
+        
+
+
 
