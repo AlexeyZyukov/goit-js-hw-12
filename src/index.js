db800ac044bf34e0c29af708a12131e4a17d5bc8
Alexey Zyukov <aalex64@ukr.net>
2021-11-18 22:28:54 +0200

    удалил лишнее

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 import './sass/main.scss';
-import '@pnotify/core/dist/BrightTheme.css';
+
 import { alert1, info, success, error1, error2 } from '../node_modules/@pnotify/core/dist/PNotify.js';
 
-import './js/fetchCountries.js';
-import './js/countryMarkup.js';
-
-// alert1 ({
-//     text: 'It is me'
-// });
-
-// error1({
-//     text: 'Проверьте правильность ввода названия страны'
-// });
-
-// error2({
-//     text: 'Слишком много совпадений. Уточните название страны'
-// });
+import './js/testJS.js';
--- src/js/refs/refs.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-export default {
-    input: document.querySelector('#text-input'),
-    result: document.querySelector('.input-message'),
-    countryList: document.querySelector('.country-container'),
-    countryName: document.querySelector('.country_name'),
-};
