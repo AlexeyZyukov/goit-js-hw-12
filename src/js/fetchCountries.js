d9556ed7592445b11bd09c4e0360446ebc354b11
Alexey Zyukov <aalex64@ukr.net>
2021-11-18 20:41:10 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -8,35 +8,60 @@ const debounce = require('lodash.debounce');
 refs.input.addEventListener('input', debounce(onInputSearch, 1000));
 refs.input.addEventListener('focus', onFocusAction, 'once');
 
+let fetchCountryByName = (name) => {
+     try {
+          return fetch(`https://restcountries.com/v2/name/${name}`)
+               .then(response => {
+                    console.dir(response);
+                    return response.json()
+               })
+     } catch (error) {
+          return console.log(error);
+     }
+};
+
+let onSuccessFetchCountryMarkup = (country) => {
+     try {
+          console.dir(country);
+          workWithSessionStorage(country);
+          // console.log(country.length); //country - массив стран, имеет св-во length
+          // console.log(country.status); // to show "response.status"
+          renderCountries();
+     } catch (error) {
+          error => console.log(error)
+     }
+};
+
 function onInputSearch() {
      const countryName = refs.input.value;
+     console.log('this is a ', this);
      // console.log(countryName);
-
      fetchCountryByName(countryName)
           .then(onSuccessFetchCountryMarkup)
      // console.log(fetchCountryByName(countryName));
 
 };
 
-function fetchCountryByName(name) {
-     return fetch(`https://restcountries.com/v2/name/${name}`)
-          // return fetch(`https://restcountries.com/v3.1/name/${name}`)
+// function fetchCountryByName(name) {
+//      return fetch(`https://restcountries.com/v2/name/${name}`)
+//           // return fetch(`https://restcountries.com/v3.1/name/${name}`)
 
-          .then(response => {
-               console.dir(response)
-               // // в промисе после вызова console.log(response.json()) передать данные дальше нельзя.
-               return response.json()
-          })
-          .catch(error => console.log(error))
-};
+//           .then(response => {
+//                console.dir(response)
+//                // // в промисе после вызова console.log(response.json()) передать данные дальше нельзя.
+//                return response.json()
+//           })
+//           .catch(error => console.log(error))
+// };
 
-function onSuccessFetchCountryMarkup(country) {
-     console.dir(country);
-     workWithSessionStorage(country);
-     // console.log(country.length); //country - массив стран, имеет св-во length
-     // console.log(country.status); // to show "response.status"
-     renderCountries();
-};
+// function onSuccessFetchCountryMarkup(country) {
+//      console.dir(country);
+//      workWithSessionStorage(country);
+//      // console.log(country.length); //country - массив стран, имеет св-во length
+//      // console.log(country.status); // to show "response.status"
+//      renderCountries();
+// };
+
 function workWithSessionStorage(array) {
      sessionStorage.setItem('country', JSON.stringify(array));
 };
@@ -80,6 +105,7 @@ function clearContent() {
 function onFocusAction(event) {
      event.preventDefault();
      onClickAction();
+     console.log('this is a ', this);
 }
 
 function onClickAction() {
@@ -185,4 +211,142 @@ function onClickAction() {
 //           </div>`
 //      }).join('');
 // };
+//===
+//=====
+//=======
+// var myMethod = function () {
+//      console.log(this);
+// };
+// // var myMethod = () => { //это определение дает при вызове undefined
+// //      console.log(this);
+// // };
+// var myObject = {
+//      myMethod: myMethod
+// };
+// myObject.myMethod() // {myMethod: ƒ}
+// //=======================
+// function foo(a) {
+//      this.a = a;
+// }
+// let bar = new foo(2);
+// console.log(bar.a); // 2
+//==================================
+// const myObject = {
+//      myArrowFunction: null,
+//      myMethod: function () {
+//           this.myArrowFunction = () => { console.log(this) };
+//      }
+// };
+// myObject.myMethod() // this === myObject - {myArrowFunction: ƒ, myMethod: ƒ}
 
+// myObject.myArrowFunction() // this === myObject - {myArrowFunction: ƒ, myMethod: ƒ}
+
+// const myArrowFunction = myObject.myArrowFunction;
+// myArrowFunction() // this === myObject
+//========================================
+// // ES6
+// var obj = {
+//      btn: document.links[0], //document.links Возвращает объект всех элементов <area> и <a>, присутствующих на странице с значением атрибута href.
+//      log: function (message) {
+//           console.log(message);
+//           return this;
+//      },
+//      init: function () {
+//           this.btn.addEventListener('click', () => this.log('Button Clicked!'), false);
+//      } //this вешает слушателя на btn, и вызывает метод log при клике на кнопку!
+// };
+// //============================================================
+// (function () {
+//      'use strict';
+//      // Переменные a и b находятся в области видимости 
+//      // самовызывающейся анонимной функции и не доступны
+//      // на более высоких уровнях
+//      var a = 10;
+//      var b = 20;
+//      // Для вывода переменной в глобальную область видимости
+//      // используется подобная конструкция
+//      window.b = b;
+// })();
+
+// // console.log(a); // undefined
+// console.log(b); // 20
+// // ==================================
+// let x = 0;
+// function foo() {
+//      let x = 1;
+//      if (x) {
+//           (function () {
+//                var x = 2;
+//                console.log(x); //2
+//           }());
+//      }
+//      console.log(x);// x всё ещё 1. 
+// }
+// console.log(x);
+// foo()
+// //======================
+// if (false) {
+//      var a = 10;
+// }
+
+// console.log(a);
+//===========================
+// {
+//      let _nested = 'secret'
+//      function nested() {
+//           return _nested
+//      }
+// }
+// console.log(nested()) //TypeError: nested is not a function
+// // nested не определена
+//================
+// var nested
+// {
+//      let _nested = 'секрет'
+//      nested = function () {
+//           return _nested
+//      }
+// }
+// console.log(nested()) // <- 'секрет'
+//===================
+// function code(groceries) {
+//      return { eat }; // эквивалентно записи const newLocal = { eat }; return newLocal;
+//      function eat() {
+//           if (groceries.length === 0) {
+//                throw new Error('Всё. Пожалуйста, купите больше продуктов, чтобы покормить код.')
+//           }
+//           return groceries.pop()
+//      }
+// }
+// const groceries = ['морковь', 'лимон', 'картофель', 'индейка']
+// console.log(code(groceries).eat()); //вызов метода eat() определенного функцией function eat()
+
+// let eater = code(groceries)
+// console.log(eater);
+// console.log(eater.eat())
+// console.log(groceries);
+//==============================
+// var foo = 1;
+// function bar() {
+//      if (!foo) {
+//           var foo = 10; //глобальная обасть видимости
+//      }
+//      alert(foo);
+// }
+// bar();//10
+//========================
+// var math = {
+//      factorial: function (num) {
+//           console.log(this);
+//           return num <= 0 ? 1 : num * math.factorial(num - 1);
+//      }
+// };
+// console.log(math.factorial(5)); // 120
+//======================
+// var run = {
+//      factorial: function factorial(num) {
+//           // console.log(this);
+//           return num <= 0 ? 1 : num * factorial(num - 1);
+//      }
+// };
+// console.log(run.factorial(5)); // 120
